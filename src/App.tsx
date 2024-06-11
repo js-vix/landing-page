7c69a8bcb89949d1a66ae15d18521246452e15d7
LucasLyra01 <lucaslyra8@gmail.com>
2024-06-10 21:12:28 -0300

    (feature) insertion of sponsors on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Layout from "./pages/Layout";
 import Footer from "./components/Footer";
+import Sponsors from "./components/Sponsors";
 
 const App = () => (
   <>
     <Navbar />
+    <Sponsors />
     <Layout />
     <Footer />
   </>
