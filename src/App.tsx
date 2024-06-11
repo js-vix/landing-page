2f65ec802b6021c43cefa937c986f7130cc553a1
Mikael Hadler <mikaelhadler@gmail.com>
2024-06-11 11:52:30 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,12 @@ import Footer from "./components/Footer";
 import Sponsors from "./components/Sponsors";
 
 const App = () => (
-<<<<<<< HEAD
-  <>
-    <Navbar />
-    <Sponsors />
-    <Layout />
-    <Footer />
-  </>
-=======
 	<>
-		<Navbar />
-		<Layout />
-		<Footer />
+	    <Navbar />
+	    <Sponsors />
+	    <Layout />
+	    <Footer />
 	</>
->>>>>>> 74df4a4438dcb89ba3ca6542bca0cd56efb50186
 );
 
 export default App;
