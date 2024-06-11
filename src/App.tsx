d8310967a61b440fda1403928c117bd1307c2619
Julian Lima <julianlimalbs@gmail.com>
2024-06-10 21:30:18 -0300

    feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar";
 import Layout from "./pages/Layout";
 import Footer from "./components/Footer";
+import Hero from "./components/hero/Hero";
 
 const App = () => (
-  <>
-    <Navbar />
-    <Layout />
-    <Footer />
-  </>
+	<>
+		<Navbar />
+		<Layout />
+		<Footer />
+		<Hero />
+	</>
 );
 
 export default App;
